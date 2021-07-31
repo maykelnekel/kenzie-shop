235198c666153d3fa181e17ed50f0a9fc9c9c713
Maykel Nekel <maykelnekel@gmail.com>
2021-07-30 23:59:19 -0300

    debug renderizando cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,6 +1,6 @@
 import { ADD_CART, DELL_CART } from "./actionTypes";
-
-const reducerCart = (state = [], action) => {
+const defaultState = JSON.parse(localStorage.getItem("cart")) || [];
+const reducerCart = (state = defaultState, action) => {
   switch (action.type) {
     case ADD_CART:
       const { product } = action;
